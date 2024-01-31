2ccbf013879b8b0148096c396ae051390eda2d0e
Till Blaha <28341894+tblaha@users.noreply.github.com>
2024-01-31 17:12:15 +0100

    debug get_logs.sh

--- support/scripts/get_logs.sh
+++ support/scripts/get_logs.sh
@@ -19,42 +19,78 @@ reset () {
     sshpass -p pi ssh -o StrictHostKeyChecking=no -o ConnectTimeout=3 pi@10.0.0.1 '/usr/bin/openocd -f /opt/openocd/openocd.cfg -c "init; reset; exit"'
 }
 
-# betaflight specific: attempt to kill connection from configurator
-which tcpkill
+# check if disk available
+sshpass -p pi ssh -o StrictHostKeyChecking=no -o ConnectTimeout=3 pi@10.0.0.1 "sudo lsblk -p | grep ${DEV}"
 if [[ $? -gt 0 ]]; then
-    echo "NOTE: tcpkill not available (install dsniff package). Manually disconnect configurator if mounting fails"
-else
-    # try to kill existing tcp connections from configurator
-    sudo timeout 3 tcpkill -9 dst host 10.0.0.1 and dst port 5761
+    # not yet available
+    echo "MSC Device not (yet) available on remote."
+
+
+    echo -n "Sending reboot-to-MSC signal to FC... "
+
+    # restart ser2net to terminate any existing connections, then hope we are faster with sending the reboot signal than auto reconnect of the configurator
+    sshpass -p pi ssh -o StrictHostKeyChecking=no -o ConnectTimeout=3 pi@10.0.0.1 "sudo systemctl restart ser2net"
+
+    # use MSP protocol over TCP (command 68, one Byte of value 0x03)
+    timeout 3 $(dirname "$0")/sendMSPoverTCP.py --host 10.0.0.1 --port 5761 68 B 3
+    if [[ $? -gt 0 ]]; then
+        echo "Failed. exiting. restart into MSC manually through the configurator"
+        exit 1
+    fi
 fi
 
-SCRIPT_PATH=$(dirname "$0")
+sleep 3
 
-# to set FC into MSC mode via MSP protocol over TCP (command 68, one Byte of value 0x03)
-$SCRIPT_PATH/sendMSP.py --host 10.0.0.1 --port 5761 68 B 3
-sleep 1
-
+echo
 sshpass -p pi ssh -o StrictHostKeyChecking=no -o ConnectTimeout=3 pi@10.0.0.1 "sudo findmnt ${DEV}"
 if [[ $? -gt 0 ]]; then
-    echo "Not yet mounted. Mounting ${DEV} on /mnt on remote..."
-    i=2
-    sshpass -p pi ssh -o StrictHostKeyChecking=no -o ConnectTimeout=3 pi@10.0.0.1 "sudo mount ${DEV} /mnt"
-    while [[ $? -gt 0  ]] && [ $i -gt 0 ]; do
+    # try to find blk device
+    i=3
+    while 
+        sshpass -p pi ssh -o StrictHostKeyChecking=no -o ConnectTimeout=3 pi@10.0.0.1 "sudo lsblk -p | grep ${DEV}"
+        [[ $? -gt 0  ]] && [[ $i -gt 0 ]]
+    do
+        echo "MSC device not (yet) available on remote. Waiting..."
         ((i=i-1))
-        sshpass -p pi ssh -o StrictHostKeyChecking=no -o ConnectTimeout=3 pi@10.0.0.1 "sudo mount ${DEV} /mnt"
+        sleep 1
+        false
     done
-fi
 
-if [[ $? -gt 0 ]]; then
-    # mount failed, only path here
-    echo "Mounting failed! Resetting FC"
-    reset
-    exit 1
+    if [[ $? -gt 0 ]]; then
+        # mount failed, only path here
+        echo "Failed to bring up MSC device. Resetting FC..."
+        echo
+        reset
+        exit 1
+    fi
+
+    echo "MSC device available! Attempting checking/fixing of FAT filesystem on ${DEV}:"
+    echo ""
+    sshpass -p pi ssh -o StrictHostKeyChecking=no -o ConnectTimeout=3 pi@10.0.0.1 "sudo fsck.vfat -l -a -v -w -V ${DEV}"
+
+    echo ""
+    echo "Attempting mounting ${DEV} on remote:/mnt..."
+    i=3
+    while
+        sshpass -p pi ssh -o StrictHostKeyChecking=no -o ConnectTimeout=3 pi@10.0.0.1 "sudo timeout 3 mount ${DEV} /mnt"
+        [[ $? -gt 0 ]] && [[ $i -gt 0 ]]
+    do
+        ((i=i-1))
+        false
+    done
+
+    if [[ $? -gt 0 ]]; then
+        # mount failed, only path here
+        echo "Mounting failed! Resetting FC..."
+        echo
+        reset
+        exit 1
+    fi
 fi
 
 exit_code=0
 
-echo "starting rsync..."
+echo "Mounting succesful! Starting rsync..."
 rsync -a --rsh "sshpass -p pi ssh -o StrictHostKeyChecking=no -l pi" --timeout=3 --delete pi@10.0.0.1:/mnt/LOGS/ "$DEST_PATH"
 if [[ $? -eq 0 ]]; then
     echo "Transfer successful! Unmounting ${DEV}..."
@@ -68,6 +104,7 @@ sshpass -p pi ssh -o StrictHostKeyChecking=no -o ConnectTimeout=3 pi@10.0.0.1 "s
 
 # always reset
 echo "Resetting FC..."
+echo
 reset
 exit ${exit_code}
 
