d0833d781c272eb7cb65800cbc295f95a27aa6c2
robinferede <robinferede@tudelft.nl>
2024-10-17 01:49:47 +0400

    added 3 trajectories for iros

---
+++ src/main/flight/trajectories/IROS_gate3->gate1/min_snap.h
@@ -0,0 +1,26 @@
+/******************************************************************************
+ *                      Code generated with SymPy 1.13.2                      *
+ *                                                                            *
+ *              See http://www.sympy.org/ for more information.               *
+ *                                                                            *
+ *                       This file is part of 'project'                       *
+ ******************************************************************************/
+
+#ifndef PROJECT__MIN_SNAP__H
+#define PROJECT__MIN_SNAP__H
+extern float maxV;
+float get_x(float t);
+float get_y(float t);
+float get_z(float t);
+float get_psi(float t);
+int get_psi_dot(float t);
+float get_vx(float t);
+float get_vy(float t);
+float get_vz(float t);
+float get_ax(float t);
+float get_ay(float t);
+float get_az(float t);
+float get_jx(float t);
+float get_jy(float t);
+float get_jz(float t);
+#endif
