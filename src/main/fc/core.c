d4985b1e7fe1c6e5a0fd17713f4668dd3b7a497a
Tom Aantjes <tom.aantjes@hotmail.com>
2025-04-01 15:08:31 +0200

    Update frequency settings

--- src/main/fc/core.c
+++ src/main/fc/core.c
@@ -1599,21 +1599,56 @@ void resetInnerLoopCounter(void) {
 //   return 0.5f + amplitude * sinf(phase); // must be between 0 and 1!
 // }
 
+// #define ACTUATOR_TEST_N_AMPLITUDES 1
+// #define ACTUATOR_TEST_N_FREQUENCIES 16
+// #define ACTUATOR_TEST_SIN_US 1500000 // 1.5 sec
+// static const float amplitudes[ACTUATOR_TEST_N_AMPLITUDES] = {0.2}; // [-]
+// static const float frequencies[ACTUATOR_TEST_N_FREQUENCIES] = {
+//      1.00, 1.82, 
+//     2.39, 3.13, 3.58, 4.1, 4.69, 5.37, 6.14, 7.03, 8.05, 
+//     9.21, 10.54, 12.07, 13.82, 15.81, 
+// }; // [Hz]
+
+
 //// Sine input settings
-#define ACTUATOR_TEST_N_AMPLITUDES 3
-#define ACTUATOR_TEST_N_FREQUENCIES 3
-#define ACTUATOR_TEST_SIN_US 3000000 // 3 sec
-static const float amplitudes[ACTUATOR_TEST_N_AMPLITUDES] = {0.1, 0.2, 0.3}; // [-]
-static const float frequencies[ACTUATOR_TEST_N_FREQUENCIES] = {0.5, 1, 2}; // [Hz]
+#define ACTUATOR_TEST_N_AMPLITUDES 1
+#define ACTUATOR_TEST_N_FREQUENCIES 16
+#define ACTUATOR_TEST_SIN_US 1500000 // 1.5 sec
+static const float amplitudes[ACTUATOR_TEST_N_AMPLITUDES] = {0.2}; // [-]
+static const float frequencies[ACTUATOR_TEST_N_FREQUENCIES] = {
+     1.00, 1.82, 
+    2.39, 3.13, 3.58, 4.1, 4.69, 5.37, 6.14, 7.03, 8.05, 
+    9.21, 10.54, 12.07, 13.82, 15.81, 
+}; // [Hz]
+
+// {18.1, 20.72, 23.72, 27.15, 
+// 31.08, 35.57, 40.72, 46.61, 53.35, 61.07, 69.9, 80.01, 91.59, 
+// 104.84, 120.0}
 
 //// Step input settings
-#define ACTUATOR_TEST_N_STEPS 3
-#define ACTUATOR_TEST_STEP_US 2000000 // 2 sec
-static const float stepInputAmplitudes[ACTUATOR_TEST_N_STEPS] = {0.25, 0.5, 0.75};    
+#define ACTUATOR_TEST_N_STEPS 9
+#define ACTUATOR_TEST_STEP_US 1000000 // 1 sec
+static const float stepInputAmplitudes[ACTUATOR_TEST_N_STEPS] = {0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};    
 
 //// Other settings
-#define ACTUATOR_TEST_BREAK_US 5000000 // 5 sec
+#define ACTUATOR_TEST_BREAK_US 3000000 // 5 sec
 #define ACTUATOR_TEST_IDLE_OUTPUT 0.1f // 10% throttle
+
+// //// Sine input settings
+// #define ACTUATOR_TEST_N_AMPLITUDES 3
+// #define ACTUATOR_TEST_N_FREQUENCIES 3
+// #define ACTUATOR_TEST_SIN_US 3000000 // 3 sec
+
+// static const float amplitudes[ACTUATOR_TEST_N_AMPLITUDES] = {0.1, 0.2, 0.3}; // [-]
+// static const float frequencies[ACTUATOR_TEST_N_FREQUENCIES] = {0.5, 1, 2}; // [Hz]
+// //// Step input settings
+// #define ACTUATOR_TEST_N_STEPS 3
+// #define ACTUATOR_TEST_STEP_US 2000000 // 2 sec
+// static const float stepInputAmplitudes[ACTUATOR_TEST_N_STEPS] = {0.25, 0.5, 0.75};    
+
+// //// Other settings
+// #define ACTUATOR_TEST_BREAK_US 5000000 // 5 sec
+// #define ACTUATOR_TEST_IDLE_OUTPUT 0.1f // 10% throttle
 
 static float testSignal(timeDelta_t timeSinceStartUs) {
     timeDelta_t totalUs = 0, deltaUs = 0;
@@ -1638,7 +1673,7 @@ static float testSignal(timeDelta_t timeSinceStartUs) {
                 // Compute sin output
                 phase = 2.0f * M_PIf * ((float) deltaUs) / periodUsFloat;
                 // return phase;
-                return 0.5f + amplitudes[i] * sinf(phase);
+                return 0.35f + amplitudes[i] * sinf(phase);
             }
 
             // If not currently doing this sine, add sine test time to total time
