90a21e7d86484dc0cc2da4a3d62f346160f18d18
Elijah Ang <elijah.anghw@gmail.com>
2026-03-03 16:59:22 +0100

    Add board and profile for 2 inch whoop

---
+++ src/main/flight/neural_controllers/neat/fig8_test_2/neural_network.h
@@ -0,0 +1,6 @@
+#ifndef NN_NETWORK_H
+#define NN_NETWORK_H
+
+void nn_forward(const float* input, float* output);
+
+#endif // NN_NETWORK_H
--- src/main/flight/neural_controllers/neural_network.h
+++ src/main/flight/neural_controllers/neural_network.h
@@ -1,6 +1,6 @@
-#ifndef NN_NETWORK_H
-#define NN_NETWORK_H
+#ifndef NEURAL_NETWORK_H
+#define NEURAL_NETWORK_H
 
 void nn_forward(const float* input, float* output);
 
-#endif // NN_NETWORK_H
+#endif // NEURAL_NETWORK_H
