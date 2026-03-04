e7af3fcc7a148c805c5c8502879785aade03cffe
Elijah Ang <elijah.anghw@gmail.com>
2026-03-04 11:18:33 +0100

    Modify Whoop2Inch profile

--- src/main/flight/neural_controllers/neural_network.h
+++ src/main/flight/neural_controllers/neural_network.h
@@ -1,6 +1,6 @@
-#ifndef NEURAL_NETWORK_H
-#define NEURAL_NETWORK_H
+#ifndef NN_NETWORK_H
+#define NN_NETWORK_H
 
 void nn_forward(const float* input, float* output);
 
-#endif // NEURAL_NETWORK_H
+#endif // NN_NETWORK_H
